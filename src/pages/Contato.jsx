c8daaf1845535706aa1fc6253b2a85caf5c1ce52
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-09-04 22:51:14 -0300

    feat: adiciona telas para seções de 'Home' e 'Contato'

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { UseNavigate, Link } from 'react-router-dom';
+
+import ContatoButton from '../components/ContatoButton';
+
+function Contato() {
+  
+  return (
+    <>
+      <h1>Olá, Me Chamo Gustavo</h1>
+      <ContatoButton/>
+    </>
+  );
+}
+
+export default Contato;
