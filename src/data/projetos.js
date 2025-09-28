ddd60fd9c587f6af0482a526d2bdfe6f4a0e9d7b
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-09-28 20:55:16 -0300

    feat: inicializa arquivo para projetos

---
+++ src/data/projetos.js
@@ -0,0 +1,25 @@
+const projetos = [
+  {
+    id: 1,
+    titulo: 'Feira solidaria',
+    descricao: 'Descrição do projeto 1',
+    github: 'https://github.com/Gustavo-Nog/feira-solidaria',
+    deploy:  ''
+  },
+  {
+    id: 2,
+    title: 'AppÁrio',
+    description: 'Descrição do projeto 2',
+    link: 'https://github.com/usuario/projeto2',
+    deploy: ''
+  },
+  {
+    id: 3,
+    title: 'Barber MVC',
+    description: 'Descrição do projeto 3',
+    link: 'https://github.com/usuario/projeto3',
+    deploy: ''
+  }
+];
+
+export default projetos;
