51df93b67506b5ceab20a4d42da242c7d2f3a87e
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-09-28 20:57:49 -0300

    feat: inicializa tela inicial do portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-// import { UseNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Home() {
   
   return (
     <>
-      <h1>Olá, Me Chamo Gustavo</h1>
-      
+      <h1 className="">Olá, me chamo Gustavo Nogueira</h1>
+      <p className="text-matrix font-matrix">Desenvolvedor FullStack</p>
+
     </>
   );
 }
-
 export default Home;
