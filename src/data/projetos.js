a67d30337d9cb5e095fdc476ff8898cf4b8b7a0d
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-09-29 23:16:34 -0300

    feat: adiciona descrição, link do repositorio no github e da hospedagem dos projetos

--- src/data/projetos.js
+++ src/data/projetos.js
@@ -2,23 +2,30 @@ const projetos = [
   {
     id: 1,
     titulo: 'Feira solidaria',
-    descricao: 'Descrição do projeto 1',
+    descricao: 'Feira Solidária é uma aplicação fullstack desenvolvida como parte do programa Capacita Brasil. O projeto tem como missão conectar pessoas, facilitando o acesso a produtos, de qualidade de forma justa e solidária.',
     github: 'https://github.com/Gustavo-Nog/feira-solidaria',
-    deploy:  ''
+    deploy:  'feira-solidaria-capacita.vercel.app'
   },
   {
     id: 2,
-    title: 'AppÁrio',
-    description: 'Descrição do projeto 2',
-    link: 'https://github.com/usuario/projeto2',
+    titulo: 'AppÁrio',
+    descricao: 'Sistema web desenvolvido com o objetivo de facilitar o gerenciamento de apiários e dados relacionados à apicultura, como pessoas responsáveis, usuários, colmeias e endereços.',
+    github: 'https://github.com/pedrohenriqux/appario',
     deploy: ''
   },
   {
     id: 3,
-    title: 'Barber MVC',
-    description: 'Descrição do projeto 3',
-    link: 'https://github.com/usuario/projeto3',
-    deploy: ''
+    titulo: 'Barber MVC',
+    descricao: 'Sistema de gerenciamento para barbearia. Permite cadastrar, editar, visualizar e excluir clientes, serviços, produtos, agendamentos e compras',
+    github: 'https://github.com/Gustavo-Nog/barber-mvc-app',
+    deploy: 'gustavo-nog.infinityfreeapp.com'
+  },
+  {
+    id: 4,
+    titulo: 'Guiz Quiz',
+    descricao: 'Um Quiz interativo. O usuário informa seu nome, escolhe um tema (como Programação, Cinema, História, etc.) e responde a 7 perguntas sorteadas aleatoriamente. O sistema calcula a pontuação final e o tempo médio de resposta.',
+    github: 'https://github.com/Gustavo-Nog/guiz-quiz',
+    deploy: 'gustavo-nog.github.io/GuizQuiz/'
   }
 ];
 
