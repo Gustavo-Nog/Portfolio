df13b3b42de5777f27ce8b43490fae6e5832b7cd
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-09-30 23:06:35 -0300

    fix: corrige uso do tailwind e de pacotes de instalação

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,15 @@ function Home() {
   
   return (
     <>
-      <h1 className="">Olá, me chamo Gustavo Nogueira</h1>
-      <p className="text-matrix font-matrix">Desenvolvedor FullStack</p>
-
+      <div>
+        <h1 className="text-brandBlue text-4xl bg-black">Tailwind funcionando</h1>
+        <p className="text-matrix text-lg underline">Desenvolvedor FullStack</p>
+        <div className="mt-4 space-y-2">
+          <div className="text-red-500">Classe built-in test (vermelho)</div>
+          <div className="text-brand-blue">Classe extend (azul)</div>
+          <div className="text-matrix">Classe extend matrix (verde)</div>
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default  {
+  darkMode: 'media',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  safelist: ['text-brand-blue','bg-brand-blue','text-matrix','bg-matrix'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue': '#0c0e82',
+        gold: '#ffb400',
+        black: '#000000',
+        white: '#ffffff',
+        gray: '#808080',
+        matrix: '#006713',
+      },
+    },
   },
   plugins: [],
 }
