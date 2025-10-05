5552d59147f6a67316047878dc4b26a6a9ba17f3
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-10-04 23:33:24 -0300

    fix: mantem wrapper com min-h-screen a ajusta viewport e centalização do conteúdo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import Home from './pages/Home.jsx';
 import Contato from './pages/Contato.jsx';
 import './App.css';
@@ -9,13 +6,14 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/contato" element={<Contato/> } />
-      </Routes>
+      <div className="min-h-screen w-full">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
-
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,11 @@
 import { Link } from 'react-router-dom';
-import { Link as ScrollLink } from 'react-scroll';
 
 function Home() {
   return (
-    <div className="bg-black h-screen w-screen flex flex-col items-center justify-center m-0 p-0">
+    <div className="min-h-screen w-full bg-black flex flex-col items-center justify-center">
       <h1 className="text-brand-blue text-4xl uppercase">Olá, me chamo Gustavo Nogueira</h1>
       <p className="text-matrix text-lg uppercase">Desenvolvedor FullStack</p>
-      <Link to="/contato">
-        Contact Me
-      </Link>
+      <Link to="/contato" className="text-white">Contact Me</Link>
     </div>
   );
 }
