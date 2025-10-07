92dabfb91b0885f3acd191e4e63dca3c11e7786f
Gustavo-Nog <gustavopan3g7@gmail.com>
2025-10-07 10:05:36 -0300

    feat: adiciona os links de deploy e github dos projetos, adiciona card dos projetos na seção

--- src/components/CardProjetos.jsx
+++ src/components/CardProjetos.jsx
@@ -1,8 +1,40 @@
+import projetos from "../data/projeto";
+import { Link } from "react-router-dom";
 
 function CardProjetos() {
     return(
-        <div>CardProjetos</div>
+        <div>
+            {projetos.map((projeto) => (
+                projeto.deploy ? (
+                    <Link 
+                        key={projeto.id} 
+                        to={projeto.deploy} 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="text-white"
+                    >
+                        <div>
+                            <h3>{projeto.titulo}</h3>
+                            <p>{projeto.descricao}</p>
+                        </div>
+                    </Link>
+                ) : (
+                    <Link 
+                        key={projeto.id} to={projeto.github} 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="text-white"
+                    >
+                        <div>
+                            <h3>{projeto.titulo}</h3>
+                            <p>{projeto.descricao}</p>
+                        </div>
+                    </Link>
+                )
+            ))}
+        </div>
     );
 }
+
 
 export default CardProjetos;
--- src/data/projeto.js
+++ src/data/projeto.js
@@ -4,7 +4,7 @@ const projetos = [
     titulo: 'Feira solidaria',
     descricao: 'Feira Solidária é uma aplicação fullstack desenvolvida como parte do programa Capacita Brasil. O projeto tem como missão conectar pessoas, facilitando o acesso a produtos, de qualidade de forma justa e solidária.',
     github: 'https://github.com/Gustavo-Nog/feira-solidaria',
-    deploy:  'feira-solidaria-capacita.vercel.app'
+    deploy:  'https://feira-solidaria-capacita.vercel.app'
   },
   {
     id: 2,
@@ -18,14 +18,14 @@ const projetos = [
     titulo: 'Barber MVC',
     descricao: 'Sistema de gerenciamento para barbearia. Permite cadastrar, editar, visualizar e excluir clientes, serviços, produtos, agendamentos e compras',
     github: 'https://github.com/Gustavo-Nog/barber-mvc-app',
-    deploy: 'gustavo-nog.infinityfreeapp.com'
+    deploy: 'https://gustavo-nog.infinityfreeapp.com'
   },
   {
     id: 4,
     titulo: 'Guiz Quiz',
     descricao: 'Um Quiz interativo. O usuário informa seu nome, escolhe um tema (como Programação, Cinema, História, etc.) e responde a 7 perguntas sorteadas aleatoriamente. O sistema calcula a pontuação final e o tempo médio de resposta.',
     github: 'https://github.com/Gustavo-Nog/guiz-quiz',
-    deploy: 'gustavo-nog.github.io/GuizQuiz/'
+    deploy: 'https://gustavo-nog.github.io/GuizQuiz/'
   }
 ];
 
